5a9f871f6bd58f503c59c6fec61513a2cacf971a
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-09-09 21:35:57 -0600

    asg1-3

--- Assignments/Asg1-3/Asg 1-3/Form1.Designer.vb
+++ Assignments/Asg1-3/Asg 1-3/Form1.Designer.vb
@@ -50,10 +50,10 @@ Partial Class Form1
         '
         'Label2
         '
-        Me.Label2.AutoSize = True
+        Me.Label2.AutoEllipsis = True
         Me.Label2.Location = New System.Drawing.Point(251, 129)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(0, 17)
+        Me.Label2.Size = New System.Drawing.Size(134, 96)
         Me.Label2.TabIndex = 2
         '
         'EarlyBirdButton
