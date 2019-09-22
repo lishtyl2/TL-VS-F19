d15930b1ce4118a56b9547ba0c435aabc57ce9db
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-09-22 00:20:16 -0600

    Updates

---
+++ Assignments/Asg3-6/ShippingCalculator.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ShippingCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.OuncesTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PoundsTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.IDCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ShippingCostTextBox = New System.Windows.Forms.TextBox()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.OuncesTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.PoundsTextBox)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.IDCodeTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 38)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(360, 296)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(284, 250)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(27, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = ".oz"
+        Me.ToolTip1.SetToolTip(Me.Label4, "How many ounces does it weigh? ")
+        '
+        'OuncesTextBox
+        '
+        Me.OuncesTextBox.Location = New System.Drawing.Point(178, 245)
+        Me.OuncesTextBox.Name = "OuncesTextBox"
+        Me.OuncesTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.OuncesTextBox.TabIndex = 5
+        Me.ToolTip1.SetToolTip(Me.OuncesTextBox, "How many ounces does it weigh? ")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(113, 248)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = ".lbs"
+        Me.ToolTip1.SetToolTip(Me.Label3, "How many pounds does it weigh?")
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 217)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(261, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Package Shipping Weight"
+        '
+        'PoundsTextBox
+        '
+        Me.PoundsTextBox.Location = New System.Drawing.Point(7, 245)
+        Me.PoundsTextBox.Name = "PoundsTextBox"
+        Me.PoundsTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.PoundsTextBox.TabIndex = 2
+        Me.ToolTip1.SetToolTip(Me.PoundsTextBox, "How many pounds does it weigh?")
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(139, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "6 Digit ID Code"
+        Me.ToolTip1.SetToolTip(Me.Label1, "Please Enter your 6 Digit ID Code here. ")
+        '
+        'IDCodeTextBox
+        '
+        Me.IDCodeTextBox.Location = New System.Drawing.Point(6, 67)
+        Me.IDCodeTextBox.MaxLength = 6
+        Me.IDCodeTextBox.Name = "IDCodeTextBox"
+        Me.IDCodeTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.IDCodeTextBox.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.IDCodeTextBox, "Please Enter your 6 Digit ID Code here. ")
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.ShippingCostTextBox)
+        Me.GroupBox2.Location = New System.Drawing.Point(409, 38)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(320, 296)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(6, 210)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(148, 25)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Shipping Cost"
+        '
+        'ShippingCostTextBox
+        '
+        Me.ShippingCostTextBox.Location = New System.Drawing.Point(10, 241)
+        Me.ShippingCostTextBox.Name = "ShippingCostTextBox"
+        Me.ShippingCostTextBox.Size = New System.Drawing.Size(218, 22)
+        Me.ShippingCostTextBox.TabIndex = 0
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.CalculateButton.Location = New System.Drawing.Point(12, 340)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(360, 98)
+        Me.CalculateButton.TabIndex = 2
+        Me.CalculateButton.Text = "&Calculate"
+        Me.ToolTip1.SetToolTip(Me.CalculateButton, "Click here to crunch those numbers. ")
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ClearButton.Location = New System.Drawing.Point(409, 340)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(115, 98)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "C&lear"
+        Me.ToolTip1.SetToolTip(Me.ClearButton, "Oops I entered wrong numbers, click here to clear everything.")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(550, 340)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(115, 98)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "E&xit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Well, I'm done now, click here to exit. ")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ShippingCalculator
+        '
+        Me.AcceptButton = Me.CalculateButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "ShippingCalculator"
+        Me.Text = "Shipping Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents OuncesTextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PoundsTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents IDCodeTextBox As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ShippingCostTextBox As TextBox
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ Assignments/Asg3-6/ShippingCalculator.vb
@@ -0,0 +1,55 @@
+﻿'Tyler Lish
+' RCET0265
+'Asg3-6
+'Shipping Calculator
+'https://github.com/lishtyl2/TL-VS-F19/tree/master/Assignments
+
+Public Class ShippingCalculator
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        Dim Pounds As Double
+        Dim Ounces As Double
+        Dim Cost As Double
+        Dim errorMessage As String
+
+        Try
+            Pounds = Double.Parse(PoundsTextBox.Text)
+        Catch anyException As Exception
+            errorMessage = errorMessage & "Please enter a numeric value in the Pounds Textbox." &
+                vbNewLine
+            PoundsTextBox.Select()
+            PoundsTextBox.Clear()
+        End Try
+        Try
+            Ounces = Double.Parse(OuncesTextBox.Text)
+        Catch anyException As Exception
+            errorMessage = errorMessage & "Please enter a numeric value in the Ounces Textbox." &
+                vbNewLine
+            OuncesTextBox.Select()
+            OuncesTextBox.Clear()
+        End Try
+        If errorMessage <> "" Then
+            MessageBox.Show(errorMessage, "Hmmm...Something's not quite right.")
+        Else
+            Ounces = Double.Parse(OuncesTextBox.Text)
+            Pounds = Double.Parse(PoundsTextBox.Text)
+        End If
+
+        Pounds = 1.92 * Pounds
+        Ounces = 0.12 * Ounces
+        Cost = Ounces + Pounds
+
+        ShippingCostTextBox.Text = Cost.ToString("C")
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ShippingCostTextBox.Text = ""
+        IDCodeTextBox.Text = ""
+        OuncesTextBox.Text = ""
+        PoundsTextBox.Text = ""
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
