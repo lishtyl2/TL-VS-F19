cbbe61a0257d2448a0bea458ea27b83e86e40656
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-10-09 08:51:56 -0600

    updates

---
+++ Examples/10-08/ListExampleForm.vb
@@ -0,0 +1,68 @@
+﻿Public Class ListExampleForm
+
+    Sub CreateList()
+        Dim namesList As New List(Of String)
+        Dim numbers As New List(Of Integer)(New Integer() {3, 5, 7, 2, 8})
+
+
+        namesList.Add("John")
+        namesList.Add("Steve")
+        namesList.Add("Basic Becky")
+        namesList.Add("Emily")
+        'Console.WriteLine(names(2))
+
+        'For Each name As String In namesList
+        '    Console.WriteLine(name)
+        'Next
+
+        'For i = 0 To namesList.Count - 1 'Step 2
+        'Console.WriteLine(namesList(i))
+        'Next
+        'For Each amount As Integer 
+        'Console.WriteLine(amount.ToString("C"))
+    End Sub
+
+    Private Sub ListExampleForm_click(sender As Object, e As EventArgs) Handles Me.Click
+        CreateList()
+
+    End Sub
+
+    Sub FormatExample()
+        'format data in nice rows and columns
+        Dim output As String
+        Dim currentString As String
+        Dim safetyNet As Integer
+        Dim startNumber As Integer
+        Dim stopNumber As Integer
+
+        output = ""
+        startNumber = 1
+        stopNumber = 10
+
+        For row = 1 To 10
+            For column = startNumber To stopNumber
+                'currentString = Str(column).Trim
+                currentString = column.ToString("C")
+                safetyNet = 0
+                'currentString.PadLeft(4)
+                Do Until currentString.Length >= 7 Or safetyNet = 100
+                    currentString = " " & currentString
+                    safetyNet = +1
+                Loop
+                output &= currentString & " |"
+
+            Next
+
+            Console.WriteLine(output)
+            stopNumber += 10
+            startNumber += 10
+            output = ""
+        Next
+
+        'Console.WriteLine(output.Length)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FormatExample()
+    End Sub
+End Class
