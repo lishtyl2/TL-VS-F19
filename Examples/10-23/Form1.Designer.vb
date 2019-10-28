459268da7ecdc2aeeb40a3d8b7505624db27dad6
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-10-28 08:44:18 -0600

    update

---
+++ Examples/10-23/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ThisButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ThisButton
+        '
+        Me.ThisButton.Location = New System.Drawing.Point(12, 309)
+        Me.ThisButton.Name = "ThisButton"
+        Me.ThisButton.Size = New System.Drawing.Size(237, 129)
+        Me.ThisButton.TabIndex = 0
+        Me.ThisButton.Text = "Ready Go"
+        Me.ThisButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ThisButton)
+        Me.Name = "Form1"
+        Me.Text = "Behind the curtain"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ThisButton As Button
+End Class
---
+++ Examples/10-23/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    'example of lists
+
+    Sub MakeANewList()
+
+        Dim myNewList As New List(Of String)
+        myNewList.Add("Hello")
+        myNewList.Add("Goodbye")
+        Console.WriteLine(myNewList(0))        Console.WriteLine(myNewList(1))
+    End Sub
+
+
+    Sub iterateList()
+        Dim otherList As New List(Of String)
+        otherList.Add("Jim")
+        otherList.Add("Bob")
+        otherList.Add("Emily")
+        otherList.Add("Beth")
+        otherList.Add("Joe")
+        otherList.Add("Emily")
+
+        'otherList.IndexOf("Emily")
+        'Console.WriteLine(otherList.IndexOf("Emily"))
+
+        'iterate through a list
+        For Each Item As String In otherList
+            'Console.WriteLine(Item.IndexOf("b"))
+            Console.WriteLine(Item & " " & "is in index : " & otherList.IndexOf(Item))
+        Next
+        removeIndexItem(3, otherList)
+        addIndexItem(3, "Sue", otherList)
+        'Console.WriteLine(otherList.Count)
+        For i = 0 To otherList.Count - 1
+            Console.WriteLine(otherList(i) & " " & "is in list index : " & Str(i))
+        Next
+
+
+        'Console.WriteLine(otherList(0))
+    End Sub
+
+    Sub removeIndexItem(ByVal index As Integer, ByRef targetList As List(Of String))
+
+        targetList.RemoveAt(index)
+
+
+    End Sub
+
+    Sub addIndexItem(ByVal index As Integer, ByVal value As String, ByRef targetList As List(Of String))
+
+        targetList.Insert(index, value)
+
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'MakeANewList()
+        iterateList()
+
+    End Sub
+End Class
