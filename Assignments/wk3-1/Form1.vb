381403f44125099e7411973958623a2e19144b03
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-11-10 23:46:52 -0700

    fixed option explicit

--- Assignments/wk3-1/Form1.vb
+++ Assignments/wk3-1/Form1.vb
@@ -3,6 +3,8 @@ Option Explicit On
 
 Public Class Form1
     Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim var As Single
+
         For i = 1 To 10
             Var = Int(Rnd() * 20) - 10
             ListBox1.Items.Insert(0, Var)
--- Assignments/wk3-2/DiceMachine.vb
+++ Assignments/wk3-2/DiceMachine.vb
@@ -4,6 +4,10 @@ Option Explicit On
 
 Public Class DiceMachine
     Private Sub DiceButton_Click(sender As Object, e As EventArgs) Handles DiceButton.Click
+        Dim var As Single
+        Dim var1 As Single
+        Dim sum As Single
+
         For i = 1 To 10000
             Var = Int(Rnd() * 6 + 1)
             var1 = Int(Rnd() * 6 + 1)
